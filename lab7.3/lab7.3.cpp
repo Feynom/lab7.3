606dd699d5e6c8cf1196c9cb9b3cf00aa902f53e
Feynom <chreptak12@gmail.com>
2020-11-14 14:28:51 +0200

    2

--- lab7.3/lab7.3.cpp
+++ lab7.3/lab7.3.cpp
@@ -10,10 +10,14 @@ using namespace std;
 void CreateMatrix_i(int** a, const int n, const int low, const int high);
 void PrintMatrix_i(int** a, const int n);
 void Search_i(int** a, int* row, int* col, const int n);
+int Suma_i(int** a, const int n, int S);
+int find_i(int** a, const int n, int num);
 //РЕКУРСИВНИЙ СПОСІБ
 void CreateMatrix_r(int** a, const int n, const int low, const int high, int i, int j);
 void PrintMatrix_r(int** a, const int n, int i, int j);
 void Search_r(int** a, int* row, int* col, const int n, int i, int num, int f, int r);
+int Suma_r(int** a, int n, int S, int i, int num);
+int find_r(int** a, const int n, int i, int num);
 
 int main()
 {
@@ -39,18 +43,21 @@ int main()
     int* Row = new int[n];
     int* Col = new int[n];
 
+                     //ІТЕРАЦІЙНИЙ СПОСІБ
     cout << "\nІТЕРАЦІЙНИЙ СПОСІБ:" << endl; cout << endl;
     CreateMatrix_i(a, n, low, high);
     PrintMatrix_i(a, n); cout << endl;
     Search_i(a, Row, Col, n); cout << endl;
-
-
-
+    cout << "\nCума елементів у тих рядках, які містять хоча б один від’ємний елемент: ";
+    cout << Suma_i(a, n, 0) << endl;
+                     //РЕКУРСИВНИЙ СПОСІБ
     cout << "\nРЕКУРСИВНИЙ СПОСІБ:" << endl; cout << endl;
     CreateMatrix_r(a, n, low, high, 0, 0);
     PrintMatrix_r(a, n, 0, 0); cout << endl;
     cout << "k =";
     Search_r(a, Row, Col, n, 0, 0, 0, 0); cout << endl;
+    cout << "\nCума елементів у тих рядках, які містять хоча б один від’ємний елемент: ";
+    cout << Suma_r(a, n, 0, 0, 0) << endl;
    
     delete[] Col;
     delete[] Row;
@@ -99,7 +106,20 @@ void Search_i(int** a, int* row, int* col, const int n)
         }
     } 
 }
-
+int Suma_i(int** a, const int n, int S)
+{
+    for (int num = 0; num < n; num++)
+        if (find_i(a, n, num) < 0)
+            for (int i = 0; i < n; i++)
+                S += a[num][i];
+    return S;
+}
+int find_i(int** a, const int n, int num)
+{
+    for (int i = 0; i < n; i++)
+        if (a[num][i] < 0)
+            return a[num][i];
+}
 //РЕКУРСИВНИЙ СПОСІБ
 
 void CreateMatrix_r(int** a, const int n, const int low, const int high, int i, int j)
@@ -140,5 +160,26 @@ void Search_r(int** a,int* row, int* col, const int n, int i, int num, int f, in
     r = 0;
     if (num < n - 1)
         return Search_r(a, row, col, n, 0, num + 1, r, f);
-
+}
+int Suma_r(int** a, int n, int S, int i, int num)
+{
+    if (i < n)
+    {
+        if (find_r(a, n, i, num) < 0)
+            S += a[num][i];
+        return Suma_r(a, n, S, i + 1, num);
+    }
+    i = 0;
+    if (num < n - 1)
+        return Suma_r(a, n, S, i, num + 1);
+    return S;
+}
+int find_r(int** a, const int n, int i, int num)
+{
+    if (i < n)
+    {
+        if (a[num][i] < 0)
+            return a[num][i];
+        return find_r(a, n, i + 1, num);
+    }
 }
